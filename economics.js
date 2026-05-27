5b6d16918b8ac7a76fc3c23b70876ea366dc6ac8
VKAUTOMATE <2020_vinyokoffi@ihs-us.org>
2026-05-26 22:28:52 -0400

    Delete economics.js

--- economics.js
+++
@@ -1,9 +0,0 @@
-import { loadEconomics } from "../../backend.js";
-
-export default async function handler(req, res) {
-  try {
-    res.status(200).json({ items: await loadEconomics() });
-  } catch (error) {
-    res.status(500).json({ error: error.message || "Economics request failed" });
-  }
-}
