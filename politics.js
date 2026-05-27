5286aaf68bf7cb74d250c2587308ec9bb58684fd
VKAUTOMATE <2020_vinyokoffi@ihs-us.org>
2026-05-26 22:29:09 -0400

    Delete politics.js

--- politics.js
+++
@@ -1,9 +0,0 @@
-import { loadPolitics } from "../../backend.js";
-
-export default async function handler(req, res) {
-  try {
-    res.status(200).json({ items: await loadPolitics() });
-  } catch (error) {
-    res.status(500).json({ error: error.message || "Politics request failed" });
-  }
-}
