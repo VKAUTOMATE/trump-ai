f30290b38f26b18c809a7a7c09f50ae29c03621f
VKAUTOMATE <2020_vinyokoffi@ihs-us.org>
2026-05-26 22:29:32 -0400

    Delete sports.js

--- sports.js
+++
@@ -1,9 +0,0 @@
-import { loadSports } from "../../backend.js";
-
-export default async function handler(req, res) {
-  try {
-    res.status(200).json({ items: await loadSports(req.query.league || "all") });
-  } catch (error) {
-    res.status(500).json({ error: error.message || "Sports request failed" });
-  }
-}
