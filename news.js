7838d6fc3a1ce75bbde45668cb19dd6d31ca87ad
VKAUTOMATE <2020_vinyokoffi@ihs-us.org>
2026-05-26 22:28:18 -0400

    Delete news.js

--- news.js
+++
@@ -1,9 +0,0 @@
-import { loadNews } from "../../backend.js";
-
-export default async function handler(req, res) {
-  try {
-    res.status(200).json({ items: await loadNews() });
-  } catch (error) {
-    res.status(500).json({ error: error.message || "News request failed" });
-  }
-}
